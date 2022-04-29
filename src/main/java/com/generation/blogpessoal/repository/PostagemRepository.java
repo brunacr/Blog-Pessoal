20df46196c7653991e19ca3c8ab85d36a32287c6
brunacr <bruna.leitao.pro@gmail.com>
2022-04-29 20:43:27 -0300

    Novos métodos adicionados

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,5 +1,8 @@
 package com.generation.blogpessoal.repository;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,6 @@ import com.generation.blogpessoal.model.Postagem;
 
 @Repository
 public interface PostagemRepository extends JpaRepository <Postagem, Long>{
-
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 }
